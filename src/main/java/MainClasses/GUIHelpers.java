05413e2f387cfdbd24d8d60315b428f9d23caba6
andrescarcia <49753315+andrescarcia@users.noreply.github.com>
2023-10-02 11:00:33 -0400

    Listo Sebas!

---
+++ src/main/java/MainClasses/GUIHelpers.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MainClasses;
+
+import GUI_Classes.Main;
+
+/**
+ *
+ * @author andre
+ */
+// clase para manejar todo lo que podriamos en las interfaces
+public class GUIHelpers {
+    private static final Main mainPage = new Main();
+    public static Main getMain() {
+        return mainPage;
+    }
+    public static void openMain() {
+        getMain().setVisible(true);
+        
+    }
+    
+}
